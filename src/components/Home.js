667628a4ed01e31b076d5d8e08c829c818faf7e3
Zach Ruiz <powersbooth10@gmail.com>
2022-09-20 20:39:44 -0500

    commit tree abb50effd1d0ad048cc763b531026807c310a695

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const divStyle = {
+    color: "firebrick"
+  }
+  return (
+    <div id="home">
+      <h1 style={divStyle}>{name} is a Web Developer from {city}</h1>
+
+    </div>
+  );
 }
 
 export default Home;
